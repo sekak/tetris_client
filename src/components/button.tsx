3b9724ca50b585bdd87eccb8e8a89f9050d1f305
sekak <ahmadesekak@gmail.com>
2026-04-12 13:55:06 +0200

    feat: implementation de Home page

---
+++ src/components/button.tsx
@@ -0,0 +1,50 @@
+import { motion, type TargetAndTransition } from 'framer-motion'
+
+interface ButtonProps {
+    text: string
+    onClick?: () => void
+    variant: 'primary' | 'secondary' | 'accent' | 'ghost'
+    disabled?: boolean
+    isAnimate?: boolean
+    delay?: number
+    initial?: boolean | TargetAndTransition | undefined
+    animate?: boolean | TargetAndTransition | undefined
+    props?: React.ButtonHTMLAttributes<HTMLButtonElement>
+}
+
+const Button = ({ text, onClick, variant, disabled = false, isAnimate = false, delay = 0, initial, animate, props }: ButtonProps) => {
+
+    const baseClasses = "w-full text-xs cursor-pointer p-3 font-pixel transition-colors"
+    const variantClasses = variant === 'primary'
+        ? "bg-primary text-gray-800 hover:bg-primary/80 box-shadow-cyan"
+        : variant === 'secondary' ? "border border-secondary text-secondary hover:bg-secondary/20"
+        : variant === 'accent' ? "bg-accent text-gray-800 hover:bg-accent/80 box-shadow-yellow"
+        : "bg-transparent text-muted hover:text-accent cursor-pointer"
+
+
+    const disabledClasses = disabled ? 'opacity-60 !cursor-not-allowed' : ''
+    const className = `${baseClasses} ${variantClasses} ${disabledClasses}`
+
+    if (isAnimate) {
+        return (
+            <motion.button
+                onClick={onClick}
+                className={className}
+                disabled={disabled}
+                initial={initial}
+                animate={animate}
+                transition={{ delay }}
+            >
+                {text}
+            </motion.button>
+        )
+    }
+
+    return (
+        <button onClick={onClick} className={className} disabled={disabled} {...props}>
+            {text}
+        </button>
+    )
+}
+
+export default Button
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,10 @@
 import { motion } from 'framer-motion'
 import bgGrid from '../assets/background-grid.jpg'
+import Interact from '../components/interact'
 
 const Home = () => {
     return (
-        <div
+        <main
             className='min-h-screen flex flex-col items-center justify-center gap-9 p-4'
             style={{
                 backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${bgGrid})`,
@@ -13,7 +14,7 @@ const Home = () => {
             }}
         >
             <motion.h1
-                className="text-5xl md:text-6xl text-primary text-glow-cyan text-center relative z-10 font-bold"
+                className="text-5xl md:text-6xl text-primary text-shadow-cyan text-center relative z-10 font-bold"
                 initial={{ y: -40, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 transition={{ duration: 0.8, ease: "easeOut" }}
@@ -22,7 +23,7 @@ const Home = () => {
             </motion.h1>
 
             <motion.p
-                className="text-md text-secondary text-glow-magenta relative z-10"
+                className="text-md text-secondary text-shadow-magenta relative z-10"
                 initial={{ y: -40, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 transition={{ delay: 0.4 }}
@@ -30,25 +31,10 @@ const Home = () => {
                 MULTIPLAYER BATTLE
             </motion.p>
 
-            <div className='flex flex-col gap-5'>
-                <motion.input
-                    initial={{ y: 40, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    transition={{ delay: 0.8 }}
-                    type='text' placeholder='Ton pseudo...'
-          className="bg-card py-3 border-border text-foreground text-center text-xs font-pixel placeholder:text-muted-foreground"
-                />
-                <motion.button
-                    initial={{ y: 40, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    transition={{ delay: 1 }}
-                    className="w-full  bg-primary text-primary-foreground hover:bg-primary/80 text-md p-3 font-pixel box-glow-cyan"
-                >
-                    Jouer
-                </motion.button>
-            </div>
-
-        </div>
+            {/* Interact component for user input and button */}
+            <Interact />
+            
+        </main>
     )
 }
 
